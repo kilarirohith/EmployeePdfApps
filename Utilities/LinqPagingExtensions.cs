61fdbcb609ef22e03b12cdabbfd70b6b4f77b897
KILARI ROHITH <116563959+kilarirohith@users.noreply.github.com>
2025-11-01 00:52:34 +0530

    Delete Utilities directory

--- Utilities/LinqPagingExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EmployeeCrudPdf.Utilities
-{
-    public static class LinqPagingExtensions
-    {
-        public static IEnumerable<T> Page<T>(this IEnumerable<T> src, int page, int pageSize)
-            => src.Skip((page - 1) * pageSize).Take(pageSize);
-    }
-}
